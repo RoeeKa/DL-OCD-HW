cbcc184bde0200e7890c0d6d1b090d81c01a1af1
Roee Kattaby <kattaby@gmail.com>
2022-11-26 22:48:12 +0200

    Push

--- data_loader.py
+++ data_loader.py
@@ -240,7 +240,7 @@ def wrapper_dataset(config, args, device):
 
         print('Preprocessing train data')
         for idx, (inputs, targets) in enumerate(train_loader):
-            inputs = F.interpolate(inputs, size=224, mode='bicubic', align_corners=False)
+            # inputs = F.interpolate(inputs, size=224, mode='bicubic', align_corners=False)
             batch = {'input':inputs,'output':targets}
             train_ds.append(deepcopy(batch))
 
@@ -250,7 +250,7 @@ def wrapper_dataset(config, args, device):
 
         print('Preprocessing test data')
         for idx, (inputs, targets) in enumerate(test_loader):
-            inputs = F.interpolate(inputs, size=224, mode='bicubic', align_corners=False)
+            # inputs = F.interpolate(inputs, size=224, mode='bicubic', align_corners=False)
             batch = {'input':inputs,'output':targets}
             test_ds.append(deepcopy(batch))
 
