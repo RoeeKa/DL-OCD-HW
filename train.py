689e521bafd6879dfef60b58dd6d45a3c963f59e
Roee Kattaby <kattaby@gmail.com>
2022-11-26 22:29:04 +0200

    Update checkpoint num

--- train.py
+++ train.py
@@ -126,6 +126,10 @@ def train(args, config, optimizer, optimizer_scale,
                         )
             optimizer_scale.step()
             optimizer_scale.zero_grad()
+
+        if idx % 100 == 0:
+            print(f'Finished batch {idx + 1}')
+
         if ((epoch + 1) % n_checkpoint == 0) or (epoch + 1 == epochs):
             print(f'epoch {epoch+1} save checkpoints: model_checkpoint_epoch{epoch}_step{step}_data{args.datatype}, scale_model_checkpoint_epoch{epoch}_loss{step}_data{args.datatype}')
             torch.save(ema_helper.state_dict(),checkpoint_path+f'model_checkpoint_epoch{epoch}_step{step}_data{args.datatype}.pt')
